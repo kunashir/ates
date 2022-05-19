81a663b0e7edce0028c66d790a100b26a9824aba
Aleksandr <kunash123@gmail.com>
2022-05-19 10:10:24 +0200

    Accounting base setup

--- accounting/app/models/transaction.rb
+++ accounting/app/models/transaction.rb
@@ -1,14 +1,24 @@
 class Transaction < ApplicationRecord
   belongs_to :account
-  validates :reason, :credit, :debit, presence: true
+  validates :reason, :credit, :debit, present: true
 
-
-  scope :today, -> { where("created_at > ?", Date.yesterday) }
+  before_create :set_prices
 
   def self.make(account:, reason:, debit: 0, credit: 0)
-    account.balance += (debit + credit)
+    if debit > 0
+      account.balance -= debit
+    elsif credit > 0
+      account.balance += credit
+    end
     account.save
     self.create(account: account, reason: reason, debit: debit, credit: credit)
   end
 
+  private
+
+  def set_prices
+    # all prices in cents
+    self.assigned_price = -1*rand(10..20) * 100
+    self.completion_price = rand(20..40) * 100
+  end
 end
